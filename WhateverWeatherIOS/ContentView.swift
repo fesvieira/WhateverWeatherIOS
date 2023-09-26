88426b2a8d36a95fd50a506457ac11f6772124b7
felipevieira <felipe2204sv@gmail.com>
2023-09-26 16:33:39 -0300

    create project

---
+++ WhateverWeatherIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhateverWeatherIOS
+//
+//  Created by Felipe Vieira on 26/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhateverWeatherIOS/WhateverWeatherIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhateverWeatherIOSApp.swift
+//  WhateverWeatherIOS
+//
+//  Created by Felipe Vieira on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WhateverWeatherIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
